596a12d7f54afce298e8765fe4c2a7330f7d8f2e
OJAH88 <jharlan628@gmail.com>
2021-11-05 10:04:30 -0400

    commit tree 8b9b29e30de69aa731261b31f98844807397d7b4

---
+++ db/migrate/20211105140144_create_artists.rb
@@ -0,0 +1,7 @@
+create_table :artists do |t|
+  # t.string is a method that takes a symbol as an argument and creates a column
+  t.string(:name)
+  t.string(:genre)
+  t.integer(:age)
+  t.string(:hometown)
+end
